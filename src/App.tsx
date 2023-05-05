55be53aecabced000ceb9ac7d4d8035035896328
Gabriel-Carvf <39717664+GabrielCarvf@users.noreply.github.com>
2023-05-05 00:55:39 -0300

    :sparkles: Criação header e input
    - Criação e estilização do header e do input/botão;

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./components/Header";
+import { PlusCircle } from "@phosphor-icons/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./global.css"
+import styles from "./App.module.css";
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header />
+      <main className={styles.content}>
+        <section className={styles.addNewTaskSection}>
+          <input placeholder="Adicione uma nova tarefa" type="text" />
+          <button>Criar <PlusCircle size={21}/></button>
+        </section>
+        <section>
+          
+        </section>
 
-export default App
+      </main>
+    </div>
+  );
+}
